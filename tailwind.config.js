52f3bce05787dbe993ba00ec8029af5932e14adf
VietCuong <aqua88hn@gmail.com>
2025-11-11 22:44:59 +0700

    Fix error clear style magin, padding

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Uncomment any plugins you want and install them as devDependencies
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+  ],
+}
